8c0804790b94cfa6133b5077df5ff8de08ed248e
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2024-05-11 14:38:53 +0330

    Create view for displaying the home page

--- core/article/views.py
+++ core/article/views.py
@@ -1,3 +1,21 @@
+from django.views.generic.edit import FormView
 from django.shortcuts import render
+from article.forms import ContactForm
 
-# Create your views here.
+
+class HomePageView(FormView):
+    """
+    View for displaying the home page and handling contact form submissions.
+    """
+
+    template_name = "index.html"
+    form_class = ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        """
+        Save the form data when it is valid.
+        """
+
+        form.save()
+        return super().form_valid(form)
