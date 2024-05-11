c7ed4c3315e5af952023f928e519b5adc2de64c5
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2024-05-11 14:29:41 +0330

    Custom admin configuration for the Contact model.

--- core/article/admin.py
+++ core/article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from article.models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Custom admin configuration for the Contact model.
+    """
+
+    date_hierarchy = "created_date"
+    list_display = ("name", "email")
+    list_filter = ("email",)
+    search_fields = ("name", "message")
+
+
+admin.site.register(Contact, ContactAdmin)
